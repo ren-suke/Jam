99b6ab37bd5f5e47cb8b3b093f2f7ff0aaaa5ed7
ren-suke <ren.git.x@gmail.com>
2024-10-06 18:43:22 +0900

    Initial Commit

---
+++ Jam/JamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamApp.swift
+//  Jam
+//
+//  Created by Ren Matsushita on 2024/10/06.
+//
+
+import SwiftUI
+
+@main
+struct JamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
